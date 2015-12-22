f36b09d002e563a9d315ad037a2ed59fd41b56bb
gsilva2718 <gisilva3108@gmail.com>
2015-12-21 19:46:29 -0700

    Testing init commit

--- app/src/main/java/electrifyingstudios/blackjacked/com/MainActivity.java
+++ app/src/main/java/electrifyingstudios/blackjacked/com/MainActivity.java
@@ -11,7 +11,7 @@ import android.view.MenuItem;
 import android.content.Intent;
 import android.widget.EditText;
 
-
+//TEST
 public class MainActivity extends AppCompatActivity {
     public final static String EXTRA_MESSAGE = "com.electrifyingstudios.blackjacked.MESSAGE";
 
